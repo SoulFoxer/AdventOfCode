34ff45105364c7554016b63b95bcb89b4d696fb8
Leon Wichelmann <leon.wichelmann@web.de>
2022-12-01 23:30:09 +0100

    init

---
+++ src/Day1.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Day1 {
+    public static void main(String[] args) throws IOException {
+
+        int max = 0;
+        int sum = 0;
+
+        File file = new File("input.txt");
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+            Scanner scanner = new Scanner(bufferedReader);
+
+            while (scanner.hasNext()) {
+                String next = scanner.nextLine();
+                if (!next.equals("\n") && !next.isEmpty()) {
+                    sum += Integer.parseInt(next);
+                } else {
+                    if (max < sum) {
+                        max = sum;
+                    }
+                    sum = 0;
+                }
+            }
+            System.out.println(max);
+        }
+    }
+}
---
+++ src/Day1Part2.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day1Part2 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        File file = new File("input.txt");
+        int[] numbers = {0, 0, 0};
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+            Scanner scanner = new Scanner(bufferedReader);
+
+            while (scanner.hasNext()) {
+                String next = scanner.nextLine();
+                if (!next.equals("\n") && !next.isEmpty()) {
+                    sum += Integer.parseInt(next);
+                } else {
+                    Arrays.sort(numbers);
+
+                    if (sum > 60000) {
+                        System.out.println("sum " + sum);
+                    }
+                    for (int i = 0; i < numbers.length; i++) {
+                        if (sum > numbers[i]) {
+                            numbers[i] = sum;
+                            break;
+                        }
+                    }
+                    sum = 0;
+                }
+            }
+
+            int total = 0;
+            for (int i : numbers) {
+                total += i;
+            }
+            System.out.println(total);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
